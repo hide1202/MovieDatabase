982d435ecdcff41529d90e780d4efad94a89c66c
Taegyeong Kim <hide1202@naver.com>
2020-06-12 00:25:51 +0900

    app 모듈에 API 시험적용

--- api/src/main/java/io/viewpoint/moviedatabase/api/MovieDatabaseApi.kt
+++ api/src/main/java/io/viewpoint/moviedatabase/api/MovieDatabaseApi.kt
@@ -1,20 +1,74 @@
 package io.viewpoint.moviedatabase.api
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import io.viewpoint.moviedatabase.api.adapter.ArrowCallAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
-class MovieDatabaseApi(private val url: String = BASE_URL) {
+class MovieDatabaseApi private constructor(
+    builder: Builder
+) {
+    private val debugLog: ((String) -> Unit)? = builder.debugLog
+    private val apiKey: String = requireNotNull(builder.apiKey) {
+        "apiKey MUST be not null"
+    }
+
     inline fun <reified T> get(): T = get(T::class.java)
 
     fun <T> get(clazz: Class<T>): T = retrofit.create(clazz)
 
     private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
-            .baseUrl(url)
+            .baseUrl(builder.url)
+            .client(OkHttpClient.Builder()
+                .apply {
+                    val logMethod = debugLog
+                    if (logMethod != null) {
+                        addInterceptor(HttpLoggingInterceptor {
+                            logMethod(it)
+                        }.apply {
+                            level = HttpLoggingInterceptor.Level.BODY
+                        })
+                    }
+
+                    addInterceptor { chain ->
+                        val request = chain.request()
+
+                        chain.proceed(
+                            request
+                                .newBuilder()
+                                .url(
+                                    request.url()
+                                        .newBuilder()
+                                        .addQueryParameter("api_key", apiKey)
+                                        .build()
+                                )
+                                .build()
+                        )
+                    }
+                }
+                .build())
             .addCallAdapterFactory(ArrowCallAdapterFactory())
-            .addConverterFactory(MoshiConverterFactory.create())
+            .addConverterFactory(
+                MoshiConverterFactory.create(
+                    Moshi.Builder()
+                        .add(KotlinJsonAdapterFactory())
+                        .build()
+                )
+            )
             .build()
+    }
+
+    class Builder {
+        var url: String = BASE_URL
+        var debugLog: ((String) -> Unit)? = null
+        var apiKey: String? = null
+
+        fun build(): MovieDatabaseApi =
+            MovieDatabaseApi(this)
     }
 
     companion object {
