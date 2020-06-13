adfd3c7ae269427a949a0c33dd9d5d9a26af5619
Taegyeong Kim <hide1202@naver.com>
2020-06-13 09:07:53 +0900

    Timber, App Startup 적용

---
+++ app/src/main/java/io/viewpoint/moviedatabase/platform/startup/TimberInitializer.kt
@@ -0,0 +1,16 @@
+@file:Suppress("unused")
+
+package io.viewpoint.moviedatabase.platform.startup
+
+import android.content.Context
+import androidx.startup.Initializer
+import timber.log.Timber
+
+class TimberInitializer : Initializer<Unit> {
+    override fun create(context: Context) {
+        Timber.plant(Timber.DebugTree())
+    }
+
+    override fun dependencies(): List<Class<out Initializer<*>>> =
+        emptyList()
+}
