69abd10c10b90aeb82e88206d16a2b16db5c2c04
Taegyeong Kim <hide1202@naver.com>
2020-07-05 07:57:49 +0900

    설정 페이지 추가. 언어 선택 이동. 메인 화면 일부 수정.

--- app/src/main/java/io/viewpoint/moviedatabase/platform/ui/MainActivity.kt
+++ app/src/main/java/io/viewpoint/moviedatabase/platform/ui/MainActivity.kt
@@ -1,22 +1,13 @@
 package io.viewpoint.moviedatabase.platform.ui
 
-import android.content.SharedPreferences
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.edit
-import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
 import io.viewpoint.moviedatabase.R
-import io.viewpoint.moviedatabase.api.MovieDatabaseApi
 import io.viewpoint.moviedatabase.databinding.ActivityMainBinding
-import io.viewpoint.moviedatabase.domain.repository.ConfigurationRepository
 import io.viewpoint.moviedatabase.platform.ui.search.MovieSearchActivity
-import io.viewpoint.moviedatabase.util.PreferencesKeys
-import kotlinx.coroutines.launch
-import javax.inject.Inject
+import io.viewpoint.moviedatabase.platform.ui.setting.SettingActivity
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -24,54 +15,14 @@ class MainActivity : AppCompatActivity() {
         DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 
-    @Inject
-    internal lateinit var preferences: SharedPreferences
-
-    @Inject
-    internal lateinit var configurationRepository: ConfigurationRepository
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        lifecycleScope.launch {
-            val languages = configurationRepository.getSupportedLanguages()
-            val adapter = ArrayAdapter(this@MainActivity,
-                R.layout.item_language
-                , languages
-                    .asSequence()
-                    .map { it.name }
-                    .filter { it.isNotEmpty() }
-                    .toList()
-            )
-
-            binding.languageSelect.setAdapter(adapter)
-
-            preferences.getString(PreferencesKeys.KEY_SELECTED_LANGUAGE_ISO, null)
-                ?.let { savedLanguageIso ->
-                    languages.firstOrNull { it.iso_639_1 == savedLanguageIso }
-                }
-                ?.let {
-                    binding.languageSelect.setText(it.name, false)
-                }
+        binding.setting.setOnClickListener {
+            startActivity(SettingActivity.intent(this))
         }
 
-        binding.languageSelect.addTextChangedListener { editable ->
-            editable ?: return@addTextChangedListener
-            lifecycleScope.launch {
-                val languages = configurationRepository.getSupportedLanguages()
-                languages.firstOrNull {
-                    it.name == editable.toString()
-                }?.let {
-                    MovieDatabaseApi.language = it.iso_639_1
-                }
-
-                preferences.edit {
-                    putString(PreferencesKeys.KEY_SELECTED_LANGUAGE_ISO, MovieDatabaseApi.language)
-                }
-            }
-        }
-
-        binding.searchButton.setOnClickListener {
+        binding.search.setOnClickListener {
             startActivity(MovieSearchActivity.intent(this))
         }
     }
---
+++ app/src/main/java/io/viewpoint/moviedatabase/platform/ui/setting/SettingActivity.kt
@@ -0,0 +1,85 @@
+package io.viewpoint.moviedatabase.platform.ui.setting
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
+import androidx.core.widget.addTextChangedListener
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
+import dagger.hilt.android.AndroidEntryPoint
+import io.viewpoint.moviedatabase.R
+import io.viewpoint.moviedatabase.api.MovieDatabaseApi
+import io.viewpoint.moviedatabase.databinding.ActivitySettingBinding
+import io.viewpoint.moviedatabase.domain.repository.ConfigurationRepository
+import io.viewpoint.moviedatabase.platform.externsion.intentToActivity
+import io.viewpoint.moviedatabase.util.PreferencesKeys
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class SettingActivity : AppCompatActivity() {
+    private val binding by lazy {
+        DataBindingUtil.setContentView<ActivitySettingBinding>(this, R.layout.activity_setting)
+    }
+
+    @Inject
+    internal lateinit var preferences: SharedPreferences
+
+    @Inject
+    internal lateinit var configurationRepository: ConfigurationRepository
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        lifecycleScope.launch {
+            val languages = configurationRepository.getSupportedLanguages()
+            val adapter = ArrayAdapter(this@SettingActivity,
+                R.layout.item_language
+                , languages
+                    .asSequence()
+                    .map { it.name }
+                    .filter { it.isNotEmpty() }
+                    .toList()
+            )
+
+            binding.languageSelect.setAdapter(adapter)
+
+            preferences.getString(PreferencesKeys.KEY_SELECTED_LANGUAGE_ISO, null)
+                ?.let { savedLanguageIso ->
+                    languages.firstOrNull { it.iso_639_1 == savedLanguageIso }
+                }
+                ?.let {
+                    binding.languageSelect.setText(it.name, false)
+                }
+        }
+
+        binding.languageSelect.addTextChangedListener { editable ->
+            editable ?: return@addTextChangedListener
+            lifecycleScope.launch {
+                val languages = configurationRepository.getSupportedLanguages()
+                languages.firstOrNull {
+                    it.name == editable.toString()
+                }?.let {
+                    MovieDatabaseApi.language = it.iso_639_1
+                }
+
+                preferences.edit {
+                    putString(PreferencesKeys.KEY_SELECTED_LANGUAGE_ISO, MovieDatabaseApi.language)
+                }
+            }
+        }
+
+        binding.close.setOnClickListener {
+            finish()
+        }
+    }
+
+    companion object {
+        fun intent(context: Context): Intent =
+            intentToActivity<SettingActivity>(context)
+    }
+}
