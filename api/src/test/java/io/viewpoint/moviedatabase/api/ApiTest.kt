91aff2b2098f86913a8f8901dc8b195165ff026f
Taegyeong Kim <hide1202@naver.com>
2020-07-05 15:53:13 +0900

    테스트 코드에 에러 메시지 추가

--- api/src/test/java/io/viewpoint/moviedatabase/api/ApiTest.kt
+++ api/src/test/java/io/viewpoint/moviedatabase/api/ApiTest.kt
@@ -5,7 +5,6 @@ import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
 import org.junit.Before
-import java.util.concurrent.TimeUnit
 
 
 abstract class ApiTest {
@@ -14,15 +13,12 @@ abstract class ApiTest {
     @Before
     fun loadWebServer() {
         mockServer.start()
-        TimeUnit.SECONDS.sleep(2)
-
         println("start webserver")
     }
 
     @After
     fun shutdownWebServer() {
         mockServer.shutdown()
-        TimeUnit.SECONDS.sleep(2)
         println("shutdown webserver")
     }
 
