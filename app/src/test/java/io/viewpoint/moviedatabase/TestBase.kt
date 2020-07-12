2e5ffa47117f33add6a201380fda13f0f3e194bb
Taegyeong Kim <hide1202@naver.com>
2020-07-12 21:05:37 +0900

    TestRule 대신 BeforeClass/AfterClass 에서 ArchTaskExecutor 설정

--- app/src/test/java/io/viewpoint/moviedatabase/TestBase.kt
+++ app/src/test/java/io/viewpoint/moviedatabase/TestBase.kt
@@ -1,23 +1,33 @@
 package io.viewpoint.moviedatabase
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.arch.core.executor.ArchTaskExecutor
+import androidx.arch.core.executor.TaskExecutor
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.setMain
+import org.junit.AfterClass
 import org.junit.BeforeClass
-import org.junit.Rule
-import org.junit.rules.TestRule
 
 abstract class TestBase {
-    @JvmField
-    @Rule
-    val rule: TestRule = InstantTaskExecutorRule()
-
     companion object {
         @JvmStatic
         @BeforeClass
         fun setUpClass() {
             Dispatchers.setMain(TestCoroutineDispatcher())
+
+            ArchTaskExecutor.getInstance().setDelegate(object : TaskExecutor() {
+                override fun executeOnDiskIO(runnable: Runnable) = runnable.run()
+
+                override fun isMainThread(): Boolean = true
+
+                override fun postToMainThread(runnable: Runnable) = runnable.run()
+            })
+        }
+
+        @JvmStatic
+        @AfterClass
+        fun tearDownClass() {
+            ArchTaskExecutor.getInstance().setDelegate(null)
         }
     }
 }
