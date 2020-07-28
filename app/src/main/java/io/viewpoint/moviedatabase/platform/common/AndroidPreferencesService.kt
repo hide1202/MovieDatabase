78bd42b223f383b4bcf5d74972c75ee6b921f44a
Taegyeong Kim <hide1202@naver.com>
2020-07-28 20:57:20 +0900

    Preference 를 Generics 을 이용하도록 수정

--- app/src/main/java/io/viewpoint/moviedatabase/platform/common/AndroidPreferencesService.kt
+++ app/src/main/java/io/viewpoint/moviedatabase/platform/common/AndroidPreferencesService.kt
@@ -15,26 +15,35 @@ class AndroidPreferencesService @Inject constructor(
         Context.MODE_PRIVATE
     )
 
-    override fun getInt(key: PreferenceKey, default: Int): Int =
-        preferences.getInt(key.key, default)
-
-    override fun getString(key: PreferenceKey): String? =
-        preferences.getString(key.key, null)
-
-    override fun putInt(key: PreferenceKey, value: Int) =
-        preferences.edit {
-            putInt(key.key, value)
-        }
-
-    override fun putString(key: PreferenceKey, value: String?) {
-        value?.let {
-            preferences.edit {
-                putString(key.key, it)
-            }
-        }
-    }
-
     companion object {
         private const val PREFERENCES_NAME = "io.viewpoint.moviedatabase.prefs"
+    }
+
+    @Suppress("UNCHECKED_CAST", "IMPLICIT_CAST_TO_ANY")
+    override fun <T : Any> getValue(key: PreferenceKey<T>, defaultValue: T?): T? =
+        when (key.type) {
+            String::class -> preferences.getString(key.key, defaultValue as String?)
+            Int::class -> preferences.getInt(key.key, defaultValue as? Int ?: 0)
+            else -> null
+        } as? T?
+
+    override fun <T : Any> putValue(key: PreferenceKey<T>, value: T?) {
+        if (value == null) {
+            if (preferences.contains(key.key)) {
+                preferences.edit {
+                    remove(key.key)
+                }
+            }
+            return
+        }
+
+        when (key.type) {
+            String::class -> preferences.edit {
+                putString(key.key, value as String)
+            }
+            Int::class -> preferences.edit {
+                putInt(key.key, value as Int)
+            }
+        }
     }
 }
