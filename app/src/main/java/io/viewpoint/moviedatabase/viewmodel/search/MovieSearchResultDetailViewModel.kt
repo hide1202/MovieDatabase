ad27f07e2bdbb3e2a1e6f61129893078bb37f352
Taegyeong Kim <hide1202@naver.com>
2020-11-01 12:56:05 +0900

    영화 상세 페이지에 장르 추가

--- app/src/main/java/io/viewpoint/moviedatabase/viewmodel/search/MovieSearchResultDetailViewModel.kt
+++ app/src/main/java/io/viewpoint/moviedatabase/viewmodel/search/MovieSearchResultDetailViewModel.kt
@@ -10,6 +10,7 @@ import arrow.core.getOrElse
 import io.viewpoint.moviedatabase.domain.repository.MovieRepository
 import io.viewpoint.moviedatabase.domain.repository.WantToSeeRepository
 import io.viewpoint.moviedatabase.domain.search.SearchResultMapper
+import io.viewpoint.moviedatabase.model.api.MovieDetail
 import io.viewpoint.moviedatabase.model.api.toMovie
 import io.viewpoint.moviedatabase.model.ui.SearchResultModel
 import io.viewpoint.moviedatabase.viewmodel.Command
@@ -22,9 +23,13 @@ class MovieSearchResultDetailViewModel @ViewModelInject constructor(
 ) : ViewModel() {
     private var result: SearchResultModel? = null
     private var _wantToSee = MutableLiveData(false)
+    private var _genres = MutableLiveData<List<MovieDetail.Genre>>(emptyList())
 
     val wantToSee: LiveData<Boolean>
         get() = _wantToSee
+
+    val genres: LiveData<List<MovieDetail.Genre>>
+        get() = _genres
 
     val invertWantToSeeCommand = Command {
         val result = result ?: return@Command
@@ -45,15 +50,17 @@ class MovieSearchResultDetailViewModel @ViewModelInject constructor(
     }
 
     suspend fun loadWithMovieId(movieId: Int): SearchResultModel? {
-        this.result = movieRepository.getMovieDetail(movieId)
+        val result = movieRepository.getMovieDetail(movieId)
+            ?.apply {
+                _genres.value = this.genres
+            }
             ?.toMovie()
             ?.let {
                 resultMapper.map(it)
             }
-        _wantToSee.value = wantToSeeRepository.hasWantToSeeMovie(movieId)
-            .attempt()
-            .suspended()
-            .getOrElse { false }
+        if (result != null) {
+            loadWithResult(result)
+        }
         return result
     }
 
@@ -63,5 +70,12 @@ class MovieSearchResultDetailViewModel @ViewModelInject constructor(
             .attempt()
             .suspended()
             .getOrElse { false }
+
+        if (_genres.value?.isEmpty() == true) {
+            movieRepository.getMovieDetail(result.id)
+                ?.let {
+                    _genres.value = it.genres
+                }
+        }
     }
 }
