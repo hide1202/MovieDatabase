2046ff4e1282cd7852ea6098cc18d59ccb36755e
Taegyeong Kim <hide1202@naver.com>
2021-04-11 19:22:10 +0900

    Retain fragments when re-create activity

--- app/src/main/java/io/viewpoint/moviedatabase/platform/ui/main/MainActivity.kt
+++ app/src/main/java/io/viewpoint/moviedatabase/platform/ui/main/MainActivity.kt
@@ -9,13 +9,14 @@ import androidx.fragment.app.commit
 import dagger.hilt.android.AndroidEntryPoint
 import io.viewpoint.moviedatabase.R
 import io.viewpoint.moviedatabase.databinding.ActivityMainBinding
-import io.viewpoint.moviedatabase.ui.setting.SettingFragment
 import io.viewpoint.moviedatabase.ui.home.HomeFragment
+import io.viewpoint.moviedatabase.ui.search.MovieSearchFragment
+import io.viewpoint.moviedatabase.ui.setting.SettingFragment
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy {
-        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,14 +25,17 @@ class MainActivity : AppCompatActivity() {
         binding.bottomNavigation.setOnNavigationItemSelectedListener {
             when (it.itemId) {
                 R.id.home_menu -> replaceFragment<HomeFragment>(HomeFragment.TAG)
-                R.id.movie_search_menu -> replaceFragment<io.viewpoint.moviedatabase.ui.search.MovieSearchFragment>(
-                    io.viewpoint.moviedatabase.ui.search.MovieSearchFragment.TAG)
-                R.id.setting_menu -> replaceFragment<io.viewpoint.moviedatabase.ui.setting.SettingFragment>(
-                    io.viewpoint.moviedatabase.ui.setting.SettingFragment.TAG)
+                R.id.movie_search_menu -> replaceFragment<MovieSearchFragment>(MovieSearchFragment.TAG)
+                R.id.setting_menu -> replaceFragment<SettingFragment>(SettingFragment.TAG)
                 else -> false
             }
         }
-        binding.bottomNavigation.selectedItemId = MainTab.HOME.itemId
+        binding.bottomNavigation.selectedItemId = savedInstanceState?.getInt(KEY_SELECTED_TAB)
+            ?.let {
+                MainTab.from(it)
+            }
+            ?.itemId
+            ?: MainTab.HOME.itemId
     }
 
     private inline fun <reified T : Fragment> replaceFragment(tag: String): Boolean {
@@ -53,5 +57,14 @@ class MainActivity : AppCompatActivity() {
             }
         }
         return true
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(KEY_SELECTED_TAB, binding.bottomNavigation.selectedItemId)
+    }
+
+    companion object {
+        private const val KEY_SELECTED_TAB = "selectedTab"
     }
 }
--- app/src/main/java/io/viewpoint/moviedatabase/platform/ui/main/MainTab.kt
+++ app/src/main/java/io/viewpoint/moviedatabase/platform/ui/main/MainTab.kt
@@ -1,18 +1,21 @@
 package io.viewpoint.moviedatabase.platform.ui.main
 
 import io.viewpoint.moviedatabase.R
-import io.viewpoint.moviedatabase.ui.setting.SettingFragment
 import io.viewpoint.moviedatabase.ui.home.HomeFragment
+import io.viewpoint.moviedatabase.ui.search.MovieSearchFragment
+import io.viewpoint.moviedatabase.ui.setting.SettingFragment
 
 enum class MainTab(
     val itemId: Int,
     val tag: String
 ) {
     HOME(R.id.home_menu, HomeFragment.TAG),
-    MOVIE_SEARCH(R.id.movie_search_menu, io.viewpoint.moviedatabase.ui.search.MovieSearchFragment.TAG),
-    SETTING(R.id.setting_menu, io.viewpoint.moviedatabase.ui.setting.SettingFragment.TAG);
+    MOVIE_SEARCH(R.id.movie_search_menu, MovieSearchFragment.TAG),
+    SETTING(R.id.setting_menu, SettingFragment.TAG);
 
-    companion object
+    companion object {
+        fun from(itemId: Int): MainTab? = values().firstOrNull { it.itemId == itemId }
+    }
 }
 
 fun MainTab.Companion.otherTab(exceptTag: String): Sequence<MainTab> =
