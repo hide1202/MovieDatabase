9bd8eb85c729e9c3ef176687a263570faea4f1e3
Taegyeong Kim <hide1202@naver.com>
2021-05-30 18:22:51 +0900

    Add a result parameter that reduces the loading time of the poster image

--- common/src/main/java/io/viewpoint/moviedatabase/binding/BindingAdapter.kt
+++ common/src/main/java/io/viewpoint/moviedatabase/binding/BindingAdapter.kt
@@ -2,16 +2,21 @@
 
 package io.viewpoint.moviedatabase.binding
 
+import android.graphics.drawable.Drawable
 import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
-import io.viewpoint.moviedatabase.resources.R
+import com.bumptech.glide.request.target.Target
 import io.viewpoint.moviedatabase.extensions.GlideBorderTransformation
+import io.viewpoint.moviedatabase.resources.R
 import io.viewpoint.moviedatabase.util.Flags
 import io.viewpoint.moviedatabase.viewmodel.Command
 import timber.log.Timber
@@ -55,6 +60,10 @@ fun ImageView.setImageUrl(
     borderColor: Int?
 ) {
     Timber.d("image url : %s", url)
+    if (tag != null && tag == url) {
+        return
+    }
+
     val fallbackImageDrawableId = if (circle == true) {
         R.drawable.fallback_circle_image
     } else {
@@ -75,6 +84,26 @@ fun ImageView.setImageUrl(
             if (circle == true) it.circleCrop()
             else it
         }
+        .listener(object : RequestListener<Drawable> {
+            override fun onLoadFailed(
+                e: GlideException?,
+                model: Any?,
+                target: Target<Drawable>?,
+                isFirstResource: Boolean
+            ): Boolean = false.also {
+                this@setImageUrl.tag = url
+            }
+
+            override fun onResourceReady(
+                resource: Drawable?,
+                model: Any?,
+                target: Target<Drawable>?,
+                dataSource: DataSource?,
+                isFirstResource: Boolean
+            ): Boolean = false.also {
+                this@setImageUrl.tag = url
+            }
+        })
         .fallback(fallbackImageDrawableId)
         .error(fallbackImageDrawableId)
         .placeholder(fallbackImageDrawableId)
--- model/src/main/java/io/viewpoint/moviedatabase/model/ui/HomeMovieListResultModel.kt
+++
@@ -1,9 +0,0 @@
-package io.viewpoint.moviedatabase.model.ui
-
-import java.io.Serializable
-
-data class HomeMovieListResultModel(
-    val id: Int,
-    val title: String,
-    val posterUrl: String?
-) : Serializable
